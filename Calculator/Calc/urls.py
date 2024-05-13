fd4ee6f52d6d173090f3672f0b6440a56525971d
DATTAJI01 <amandatta01@gmail.com>
2024-05-13 16:57:26 +0530

    Add new configuration

--- Calculator/Calc/urls.py
+++ Calculator/Calc/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name="home"),
+    path('',views.main,name="main"),
+    path('calculate/',views.calculate,name="calculate"),
 ]
--- Calculator/Calc/views.py
+++ Calculator/Calc/views.py
@@ -5,3 +5,32 @@ from django.http import HttpResponse
 def main(request):
     context={}
     return render(request,'Calculator.html',context)
+
+
+def calculate(request):
+    if request.method == 'POST':
+        First_value = request.POST.get('First_value')
+        Second_value = request.POST.get('Second_value')
+        Operation = request.POST.get('Operation')
+
+        if Operation == 'add':
+            result = eval(First_value) + eval(Second_value)
+            context = {'result': result}
+            return render(request,'Calculator.html',context)
+        
+        elif Operation == 'subtract':
+            result = eval(First_value) - eval(Second_value)
+            context = {'result': result}
+            return render(request,'Calculator.html',context)
+        
+        elif Operation == 'multiply':
+            result = eval(First_value) * eval(Second_value)
+            context = {'result': result}
+            return render(request,'Calculator.html',context)
+        
+        elif Operation == 'divide':
+            result = eval(First_value) / eval(Second_value)
+            context = {'result': result}
+            return render(request,'Calculator.html',context)
+    else:
+        return HttpResponse('Calculator.html')
